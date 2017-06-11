c4cef6aab01e783858c6508e46519d51117c517c
ChiWeiHsiao <kiwi010379@gmail.com>
2017-06-11 22:47:51 +0800

    change from atari game to simple control game

--- game_wrapper.py
+++ game_wrapper.py
@@ -10,10 +10,9 @@ class Game():
         self.env =  gym.make(game_name)
         #self.env = wrappers.Monitor(self.env, 'records/atari-experiment-1')
         self.n_actions = self.env.action_space.n
-        self.resize_width = 80
-        self.resize_height = 80
-        self.histoy_length = 4  # One state contains 'histoy_length' frames
-        self.state_buffer = deque() # Buffer keep 'histoy_length-1' frames
+        self.n_observation = 2 #?
+        self.histoy_length = 4  # One state contains 'histoy_length' observations
+        self.state_buffer = deque() # Buffer keep 'histoy_length-1' observations
 
     def initial_state(self):
         ''' Initialize game. Prepare initial state and state_buffer
@@ -23,61 +22,48 @@ class Game():
         '''
         self.state_buffer = deque()  # Clear the state buffer
         observation = self.env.reset()
-        frame = self.preprocess_frame(observation)
-        single_frame = np.reshape(frame, (self.resize_width, self.resize_height, 1))
-        state = single_frame
+        observation_reshaped = np.stack([observation], axis=0)
+        state = observation_reshaped
         for i in range(self.histoy_length-1):
-            state = np.concatenate((state, single_frame), axis=-1) 
-        assert state.shape == (self.resize_width, self.resize_height, self.histoy_length)
-        # Prepare 'histoy_length-1' frames in buffer
+            state = np.concatenate((state, observation_reshaped), axis=0) 
+        # Prepare 'histoy_length-1' observations in buffer
         for i in range(self.histoy_length-1):
-            self.state_buffer.append(frame)
+            self.state_buffer.append(observation)
+        print('ori observation:', len(observation))
+        print('observation_reshaped:', observation_reshaped.shape)
+        print('state:', state.shape)
         return state
 
     def step(self, action):
         ''' Execute the given action, move to next time point
         Execute one action in the game. Transition from state_t to state_t1, with immediate reward.
-        Build current state ( = previous 'histoy_length-1' frames + current frame ).
-        Pop out the oldest frame, push latest frame into state_buffer.
+        Build current state ( = previous 'histoy_length-1' observations + current observation ).
+        Pop out the oldest observation, push latest observation into state_buffer.
         '''
         observation_t1, reward, terminal_t1, info = self.env.step(action)
-        observation_t1 = self.preprocess_frame(observation_t1)
-
-        previous_frames = np.array(self.state_buffer) #(3, 80, 80)
-        state_t1 = np.empty((self.resize_width, self.resize_height, self.histoy_length))
-        for i in range(previous_frames.shape[0]):
-            state_t1[..., i] = previous_frames[i]
-        state_t1[..., self.histoy_length-1] = observation_t1
-        assert state_t1.shape == (self.resize_width, self.resize_height, self.histoy_length)
-        # Pop the oldest frame, add the current frame to the queue
+        previous_observations = np.array(self.state_buffer) #(3, 4)
+        state_t1 = np.empty((self.histoy_length, self.n_observation))
+        for i in range(previous_observations.shape[0]):
+            state_t1[i, ...] = previous_observations[i]
+        state_t1[self.histoy_length-1, ...] = observation_t1
+        # Pop the oldest observation, add the current observation to the queue
         self.state_buffer.popleft()
         self.state_buffer.append(observation_t1)
 
         return state_t1, reward, terminal_t1, info
 
-    def preprocess_frame(self, observation):
-        ''' Preprocess screen image data
-        Color is not important here, so change RGB to gray scale.
-        Resize the smaller image for training efficiency.
-        '''
-        gray_img = np.dot(observation[...,:3], [0.299, 0.587, 0.114])
-        gray_img = Image.fromarray(gray_img)
-        resized_img = gray_img.resize((self.resize_width , self.resize_height), Image.BILINEAR )
-        #result = np.reshape(resized_img, (self.resize_width , self.resize_height, 1))
-        result = np.reshape(resized_img, (self.resize_width , self.resize_height))
-        return result
 
     def random_action(self):
         return self.env.action_space.sample()
 
     def show_game_info(self):
         ''' Show information about the game'''
-        print('Action space: {}'.format(self.env.action_space)) # Discrete(6) => 6 actions,  either 0 or 1
-        print('Observation space: {}'.format(self.env.observation_space)) # Box(250, 160, 3), rgb
+        print('Action space: {}'.format(self.env.action_space))
+        print('Observation space: {}'.format(self.env.observation_space))
 
 
 def example():
-    game = Game('AirRaid-v0')
+    game = Game('MountainCar-v0')
     game.show_game_info()
     init_state = game.initial_state()
     game_over = False
@@ -87,6 +73,7 @@ def example():
             game.env.render()
             action = game.env.action_space.sample()
             state, reward, game_over, info = game.step(action)
+            print('state:', state.shape)
         game.env.reset()
 
 
