ae0eb7f0040e03efca8a7e2b54613672ba322e33
Jevan Chan <jevan0307@gmail.com>
2017-06-15 16:33:58 +0800

    Remove deubg message

--- memory.py
+++ memory.py
@@ -107,8 +107,6 @@ class Memory(object):   # stored as ( s, a, r, s_ ) in SumTree
         if self.len < self.capacity:
             self.len = self.len + 1
 
-        print('Store', self.len)
-
         if self.enable_pri:
             max_p = np.max(self.tree.tree[-self.tree.capacity:])
             if max_p < self.abs_err_upper:
@@ -119,7 +117,6 @@ class Memory(object):   # stored as ( s, a, r, s_ ) in SumTree
             self.tree.data_pointer = (self.tree.data_pointer + 1) % self.capacity
 
     def sample(self, n):
-        print('Sample')
         if self.enable_pri:
             batch_idx, batch_memory, ISWeights = [], [], []
             segment = self.tree.root_priority / n
